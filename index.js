c78dff1412808dc44bb858fea8e892aa65bd46ef
Atul Bhardwaj <atulbhardwaj@Atuls-MacBook-Pro.local>
2021-04-30 20:48:16 +0530

    more configrations

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const { reMapTransports } = require('./src/transportGenerator');
+const configurations = require('./config.json');
 
-module.exports = reMapTransports;
+module.exports = {
+    reMapTransports,
+    configurations
+}
